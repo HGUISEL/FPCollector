d3f4263056da2338de6e6358df0cd3a6e4afa001
YoonhoChoi96 <uenho830@gmail.com>
2019-07-28 19:28:21 +0900

    AST setting and keep working with get Control Flow

--- src/main/edu/handong/csee/isel/fpcollector/contextextractor/astvector/DataDependentLineGetter.java
+++ src/main/edu/handong/csee/isel/fpcollector/contextextractor/astvector/DataDependentLineGetter.java
@@ -11,7 +11,7 @@ public class DataDependentLineGetter {
 		ArrayList<String> dpLines = new ArrayList<>();
 		
 		try {
-			
+			int FileNum = 0;
 			for(String pathVariable : pathLineErrVar) {
 				String path = pathVariable.split(",")[0];
 				String var = pathVariable.split(",")[3].trim();
@@ -22,24 +22,87 @@ public class DataDependentLineGetter {
 				
 				String line = "";
 				String lineInfo = ",%%%%%";
+				ArrayList<Integer> controlFlow = new ArrayList<>();
+				ArrayList<Integer> dataDependency = new ArrayList<>();
+				ArrayList<String> blocks = new ArrayList<>();
+				ArrayList<String> blockPairs = new ArrayList<>();
+				//ArrayList<Integer> endBlock = new ArrayList<>();
+				
 				int lineNum = 0;
+				//for ArrayList.get(i), initiate as -1
+				int blockNum = -1;
+				int blockPairNum = 0;
+				int controlNum =0 ;
 				
 				while((line = bufReader.readLine()) != null) {
 					lineNum ++;
-					int startIdx = line.indexOf(var);
-					//int endIdx = startIdx + var.length();
+					if(!line.trim().startsWith("*") && 
+							!line.trim().startsWith("/*") &&
+							!line.trim().startsWith("//") &&
+							(line.matches(".+\\bif\\b.+") || line.contains(".+\\belse\\b.+") 
+							|| line.contains(".+\\bwhile\\b.+") || line.contains(".+\\bfor\\b.+")
+							|| line.contains(".+\\bswitch\\b.+") || line.contains(".+\\btry\\b.+")
+							|| line.contains(".+\\bcatch\\b.+"))) {
+						controlFlow.add(lineNum);
+						controlNum ++;
+					}
+					
+					if(lineNum == 36) {
+						System.out.println("a");
+					}
+					
+					if(controlNum > 0 && line.matches(".+\\{.+\\}.+")&&
+							!line.trim().substring(0, 1).contains("*") && 
+							!line.trim().substring(0, 1).contains("/*") &&
+							!line.trim().substring(0, 1).contains("//")) {
+						String blockPair = "" + lineNum + "-" + lineNum;
+						blockPairs.add(blockPairNum, blockPair);
+						blockPairNum ++;
+					} else if(true) 
+						{if (controlNum > 0 &&
+							line.contains("}") &&
+							!line.trim().startsWith("*") && 
+							!line.trim().startsWith("/*") &&
+							!line.trim().startsWith("//")) {
+								String blockPair = blocks.get(blockNum) + "-" + lineNum;
+								blocks.remove(blockNum);
+								blockPairs.add(blockPairNum, blockPair);
+								blockNum --;
+								controlNum --;
+								blockPairNum ++;
+					} if(controlNum > 0 &&
+							line.contains("{") &&
+							!line.trim().startsWith("*") && 
+							!line.trim().startsWith("/*") &&
+							!line.trim().startsWith("//")) {
+								blockNum ++;
+								blocks.add(""+lineNum);
+						}
+					}
+					
 					String lineMatches= ".+\\b"+var+"\\b.+";
 					if(line.matches(lineMatches)) {
 						lineInfo = lineInfo.concat("" + lineNum + ") " + line + "\n");
+						dataDependency.add(lineNum);
 					}
+					
+					System.out.println("line" + lineNum);
 				}
+				FileNum ++;
+				System.out.println(FileNum);
+				if(FileNum == 16)
+					System.out.println(FileNum);
+				
 				bufReader.close();
-				dpLines.add(pathVariable + lineInfo);
+				
+//				for(int i = 0 ; i < lineNum; i ++) {
+//					if(startBlock.get(i))
+//				}
+				//dpLines.add(pathVariable + lineInfo);
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-		
 		return dpLines;
 	}
 }
--- src/main/edu/handong/csee/isel/fpcollector/contextextractor/astvector/ViolationControlFlowGetter.java
+++ src/main/edu/handong/csee/isel/fpcollector/contextextractor/astvector/ViolationControlFlowGetter.java
@@ -8,35 +8,43 @@ import java.util.ArrayList;
 
 public class ViolationControlFlowGetter {
 	public ArrayList<String> getControlFlow(ArrayList<String> dirLineErrVarDataDependLine){
-		ArrayList<String> abstractedContext = new ArrayList<>();
+		ArrayList<String> controlFlowStatements = new ArrayList<>();
 		
-		try {
+try {
 			
-			for(String pathLine : dirLineErrVarDataDependLine) {
-				String tempPath = pathLine.split(",")[0];
-				String tempLine = pathLine.split(",")[1].trim();
-			
-				File f = new File(tempPath);
+			for(String pathVariable : dirLineErrVarDataDependLine) {
+				String path = pathVariable.split(",")[0];
+				String var = pathVariable.split(",")[3].trim();
+				String dataDependLine = pathVariable.split(",%%%%%")[1].trim();
+				
+				String dependLines[] = dataDependLine.split("\n");
+				String lineNumber = "";
+				ArrayList<Integer> dependentLines = new ArrayList<>();
+				
+				for(String lines : dependLines) {
+					lineNumber = lines.substring(0, 5).split(")")[0];
+					dependentLines.add(Integer.parseInt(lineNumber));
+				}
+				
+				String line = "";
+				
+				File f = new File(path);
 				FileReader fReader = new FileReader(f);
 				BufferedReader bufReader = new BufferedReader(fReader);
 				
-				String line = "";
-				String lineInfo = ",%%%%%";
 				int lineNum = 0;
 				
 				while((line = bufReader.readLine()) != null) {
-					lineNum ++;
-					if(line.contains(var)) {
-						lineInfo = lineInfo.concat("" + lineNum + ") " +line + "\n");
-					}
+				
 				}
+				
+				
 				bufReader.close();
-				dpLines.add(pathVariable + lineInfo);
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
 		
-		return abstractedContext;
+		return controlFlowStatements;
 	}
 }
