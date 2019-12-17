1af4424a232f1f77d278f42da0170d762601bc5f
YoonhoChoi96 <uenho830@gmail.com>
2019-12-17 14:54:18 +0900

    update DFS

--- src/main/edu/handong/csee/isel/fpcollector/refactoring/BNFChecker.java
+++ src/main/edu/handong/csee/isel/fpcollector/refactoring/BNFChecker.java
@@ -1,16 +1,24 @@
 package edu.handong.csee.isel.fpcollector.refactoring;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
+import org.eclipse.jdt.core.dom.ChildPropertyDescriptor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SimplePropertyDescriptor;
 
 public class BNFChecker {
 	Info info = new Info();
 	ArrayList<SimpleName> violatedNode = new ArrayList<>();
 	public ArrayList<MethodAST> methodASTs = new ArrayList<>();
 	ArrayList<PatternNode> patterns = new ArrayList<>();
+	ArrayList<ASTNode> children = new ArrayList<>();
 	
 	public void run(Info info, PatternVector patternVector) {
 		this.info = info;
@@ -18,60 +26,39 @@ public class BNFChecker {
 	}
 	
 	private void buildAST(PatternVector patternVector) {
+		int start = 0;
+		int end = 0;
+		ArrayList<MethodDeclaration> methods = new ArrayList<>();
 		JavaASTParser javaParser = new JavaASTParser(info.source);
 		violatedNode = javaParser.getViolatedNames(info.varName);
-		
-		checkInRange(patternVector);
+		start = Integer.parseInt(info.start);
+		end = Integer.parseInt(info.end);
+		methods.addAll(javaParser.getMethodDeclarations());
+		checkInRange(patternVector, start, end, methods);
 	}
 	
-	private void checkInRange(PatternVector patternVector) {
-		int flag = 1;
-		int start = 0;
+	private void checkInRange(PatternVector patternVector, int start, int end, ArrayList<MethodDeclaration> methods) {
 		int methodStart = 0;
-		int end = 0;
-//		String tempSource = "" + info.source;
-		
-//		String[] lines = tempSource.split("\n");
-//		String tempLine = lines[Integer.parseInt(info.start) -1];
-//		start = tempSource.indexOf(tempLine);
-//		tempLine = lines[Integer.parseInt(info.end) - 1];
-//		tempSource = tempSource.substring(start);
-//		end = tempSource.indexOf(tempLine) + start;
-		
-		if(flag == 1) {
-		for(int i =0 ; i < Integer.parseInt(info.start) -1; i ++) {
-			info.source = info.source.replaceFirst("\n", "/*NewLineOccurred*/");
-		}
-		start = info.source.lastIndexOf("/*NewLineOccurred*/") + 19;
-		for(int i = 0; i < Integer.parseInt(info.end) - Integer.parseInt(info.start) - 1; i ++) {
-			info.source = info.source.replaceFirst("\n", "/*NewLineOccurred*/");
-		}
-		end = info.source.lastIndexOf("/*NewLineOccurred*/") + 19;
-		flag--;
-		}
-		
-		JavaASTParser parserInRange = new JavaASTParser(info.source, start, end);
-		ArrayList<MethodDeclaration> methods = parserInRange.getMethodDeclarations();
-		
-		int methodHit = 0;
-		for(MethodDeclaration tempMethod : methods) {
-			int tempMethodPosition = tempMethod.getStartPosition();
-			if(tempMethodPosition <= end && tempMethodPosition >= start) {
-//				System.out.println(tempMethod);
-				methodHit = 1;
-			}
-		}
 		int min = 99999999;
-		if(methodHit == 0 ) {
-			
-			for(MethodDeclaration tempMethod : methods) {
-				if(tempMethod.getStartPosition() <= start && start - tempMethod.getStartPosition() < min) {
-					min = start - tempMethod.getStartPosition();
-					methodStart = tempMethod.getStartPosition();
-				}
+		JavaASTParser parserInRange;
+		ASTNode methodInRange = null;
+		
+		for(MethodDeclaration tempMethod : methods) {
+			int lineNumber = ((CompilationUnit) tempMethod.getRoot()).getLineNumber(tempMethod.getStartPosition()) - 1;
+			if(lineNumber <= start && start - lineNumber < min) {
+				min = start - lineNumber;
+				methodStart = lineNumber;
+				methodInRange = tempMethod;
 			}
-			parserInRange = new JavaASTParser(info.source, methodStart, end);
 		}
+		
+		printChild(methodInRange, children);
+		
+		for(ASTNode tempnode : children) {
+			System.out.println(tempnode.getClass().getSimpleName());
+		}
+		
+		parserInRange = new JavaASTParser(info.source, methodStart, end);
 		
 		for(ASTNode temp : parserInRange.getInRangeNode()) {
 			System.out.println(temp.getClass().getSimpleName());
@@ -115,6 +102,43 @@ public class BNFChecker {
 			System.out.println(nod.node.getClass().getSimpleName() + "(" + nod.count + ")");
 		}
 	}
+	
+	@SuppressWarnings("rawtypes")
+	private static void printChild(ASTNode node, ArrayList<ASTNode> children) {
+    	List properties = node.structuralPropertiesForType();
+    	
+    	for(Iterator itertor = properties.iterator(); itertor.hasNext();) {
+    		Object desciptor = itertor.next();
+    		if(desciptor instanceof SimplePropertyDescriptor) {
+    			SimplePropertyDescriptor simple = (SimplePropertyDescriptor)desciptor;
+    			Object value = node.getStructuralProperty(simple);
+    			System.out.println(simple.getId() + " SimpleProperty (" + value.toString() + ")");
+    		} else if (desciptor instanceof ChildPropertyDescriptor) {
+    			ChildPropertyDescriptor child = (ChildPropertyDescriptor) desciptor;
+    			ASTNode childNode = (ASTNode) node.getStructuralProperty(child);    			
+    			if(childNode != null) {
+    				System.out.println("Child ( " + child.getId() + ") {");
+    				children.add(childNode);
+    				printChild(childNode, children);
+    				System.out.println("}\n");
+    			}
+    		} else { 
+    			ChildListPropertyDescriptor list = (ChildListPropertyDescriptor) desciptor;
+    			System.out.println("ChildList (" + list.getId()+ ") {");
+    			printChild((List)node.getStructuralProperty(list), children);
+    			System.out.println("}\n");
+    		}
+    	}
+    }
+	
+	 @SuppressWarnings("rawtypes")
+		private static void printChild(List nodes, ArrayList<ASTNode> children) {
+	    	for ( Iterator iterator = nodes.iterator(); iterator.hasNext();) {
+	    		ASTNode node = (ASTNode) iterator.next();
+	    		children.add(node);
+	    		printChild(node, children);
+	    	}
+	    }
 	
 	
 }
